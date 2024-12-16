01b4cd47b61d24ef8d84639a4f7177151aca462a
IvanPanico <ivan.panico@resilience.care>
2024-12-16 12:19:12 +0100

    day 5

---
+++ 2024/5/part1.cpp
@@ -0,0 +1,66 @@
+#include <chrono>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+
+using namespace std;
+
+int main() {
+  const auto start = chrono::high_resolution_clock::now();
+  ifstream myfile("data.txt");
+  auto result = 0;
+  string line;
+  string number;
+  auto update_section = false;
+  map<int, set<int>> order_rules;
+
+  if (myfile.is_open()) {
+    while(getline(myfile, line)) {
+      istringstream ss(line);
+      vector<int> numbers;
+      vector<int> ordered_numbers;
+      auto valid_update = true;
+      if (line.empty()) {
+        update_section = true;
+        continue;
+      }
+      if (update_section) {
+        while(getline(ss, number, ','))
+          numbers.push_back(stoi(number));
+        
+        for (auto it = numbers.rbegin(); it != numbers.rend(); ++it) {
+          for (auto jt = it + 1; jt != numbers.rend(); ++jt) {
+            if (order_rules[*it].contains(*jt)) {
+              valid_update = false;
+              break;
+            }
+          }
+
+        }
+
+        if (valid_update) {
+          result += numbers[(int)numbers.size() / 2];
+        }
+
+      } else {  
+        while(getline(ss, number, '|'))
+          numbers.push_back(stoi(number));
+        order_rules[numbers[0]].insert(numbers[1]);
+      }
+    }
+    cout << result << endl;
+    const auto end = chrono::high_resolution_clock::now();
+    const auto time_elapsed =
+        chrono::duration_cast<std::chrono::microseconds>(end - start)
+            .count();
+    cout << "Time in us: " << time_elapsed << endl;
+  }
+
+  else
+    cout << "Unable to open file";
+
+  return 0;
+}
